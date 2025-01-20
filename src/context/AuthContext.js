90d036cdd44be4c424a87f9394b3c3c32b673a9c
Deven-14 <deven.paramaj@gmail.com>
2025-01-20 23:48:27 +0530

    fix(auth): signin with popup inital setup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,14 @@
 import { useContext, createContext, useEffect, useState } from 'react';
 import {
   GoogleAuthProvider,
-  signInWithRedirect,
+  // signInWithRedirect,
+  signInWithPopup,
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
 import { auth } from './../config/firebase';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { upsertUser } from '../api/user';
+// import { upsertUser } from '../api/user';
 
 
 const AuthContext = createContext();
@@ -20,40 +21,48 @@ export const AuthContextProvider = ({ children }) => {
   const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     // provider.addScope("https://www.googleapis.com/auth/devstorage.read_only")
-    signInWithRedirect(auth, provider);
+    // signInWithRedirect(auth, provider);
+    const userCred = await signInWithPopup(auth, provider);
+    const user = userCred.user;
+    // upsert user here
+    console.log("here", await user.getIdToken());
   };
 
   const googleSignOut = async () => {
     await signOut(auth);
   };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+  const initializeUser = async (currentUser) => {
+    console.log('User signed in / out', currentUser);
 
-      console.log('User signed in / out', currentUser);
-      setUser(currentUser);
+    if (currentUser && currentUser.uid) {
+      try {
 
-      if (currentUser && currentUser.uid) {
-        try {
+        // const token = await currentUser.getIdToken();
+        // const response = await upsertUser(token);
+        // console.log('upsertUser response', response);
+        // retrieve user data here
+        setUser(currentUser);
+        navigate(location.pathname, { replace: true });
 
-          const token = await currentUser.getIdToken();
-          const response = await upsertUser(token);
-          console.log('upsertUser response', response);
-          navigate(location.pathname, { replace: true });
+      } catch (error) {
 
-        } catch (error) {
-          console.log('upsertUser error', error);
-          await signOut(auth);
-        }
-      } else {
-        navigate('/', { replace: true });
+        console.log('unable to retrieve user details', error);
+        await signOut(auth);
       }
 
-    });
-    
-    return () => {
-      unsubscribe();
+    } else {
+
+      setUser({});
+      navigate('/', { replace: true });
     }
+
+  }
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
